fb8af4c33911c0416d9e831d570d9b9daed83f02
Nayetdet <jpmoreiracampos21@gmail.com>
2025-05-01 05:26:59 -0300

    feat: adicionados arquivos base do projeto

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <benchmark/benchmark.h>
+#include <chrono>
+#include <iostream>
+#include <random>
+
+constexpr unsigned int N = 128;
+constexpr unsigned int NUM_ITERATIONS = 2500;
+
+static void matmul() {
+    volatile double a[N][N];
+    volatile double b[N][N];
+    volatile double c[N][N];
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+    for (unsigned int i = 0; i < N; i++) {
+        for (unsigned int j = 0; j < N; j++) {
+            a[i][j] = dist(gen);
+            b[i][j] = dist(gen);
+            c[i][j] = 0.0;
+        }
+    }
+
+    for (unsigned int i = 0; i < N; i++) {
+        for (unsigned int j = 0; j < N; j++) {
+            for (unsigned int k = 0; k < N; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+static void BM_matmul(benchmark::State& state) {
+    auto start = std::chrono::high_resolution_clock::now();
+    for (auto _ : state) {
+        matmul();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+    state.counters["TotalTimeSec"] = benchmark::Counter(elapsed_seconds.count(), benchmark::Counter::kDefaults);
+    state.counters["IterationsPerSec"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+}
+
+BENCHMARK(BM_matmul)
+    ->Iterations(NUM_ITERATIONS)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
