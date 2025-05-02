b9a1553f810705c7053cde789a4b5e310d7e1076
Nayetdet <jpmoreiracampos21@gmail.com>
2025-05-01 21:57:13 -0300

    fix: ajustadas contas dos resultados para multithread

--- main.cpp
+++ main.cpp
@@ -42,12 +42,27 @@ static void BM_matmul(benchmark::State& state) {
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start) / state.threads();
     state.counters["TotalTimeSec"] = benchmark::Counter(elapsed_seconds.count(), benchmark::Counter::kDefaults);
-    state.counters["IterationsPerSec"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+    state.counters["IterationsPerSec"] = benchmark::Counter(state.iterations() * state.threads(), benchmark::Counter::kIsRate);
 }
 
-BENCHMARK(BM_matmul)
-    ->Unit(benchmark::kMillisecond)
-    ->Iterations(NUM_ITERATIONS)
-    ->ThreadRange(1, std::thread::hardware_concurrency());
+int main(int argc, char **argv) {
+    const unsigned int maxThreads = std::thread::hardware_concurrency();
+    for (unsigned int i = 1; i < maxThreads; i *= 2) {
+        benchmark::RegisterBenchmark("BM_matmul", BM_matmul)
+            ->Iterations(NUM_ITERATIONS / i)
+            ->Unit(benchmark::kMillisecond)
+            ->Threads(i);
 
-BENCHMARK_MAIN();
+        if (i * 2 > maxThreads && i != maxThreads) {
+            benchmark::RegisterBenchmark("BM_matmul", BM_matmul)
+                ->Iterations(NUM_ITERATIONS / maxThreads)
+                ->Unit(benchmark::kMillisecond)
+                ->Threads(maxThreads); 
+        }
+    }
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+
+    return 0;
+}
